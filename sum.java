b846a2f3c31268250daf3d7e297923fd2cd723af
abbadasarishravani <160824306+abbadasarishravani@users.noreply.github.com>
2024-02-26 21:44:12 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,17 @@
+//This program is to print the sum of 1 to n natural numbers
+import java.io.*;
+class sum
+{
+public static void main(String args[])
+{
+int n = 100;
+int i= 1;
+int sum = 0;
+while(i<=n)
+{
+sum = sum+i;
+i++;
+}
+System.out.println(sum);
+}//main
+}//sum
