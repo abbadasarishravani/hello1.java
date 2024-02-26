c77f32989211988bd61be8a2cfd8d5cba7a8cd83
abbadasarishravani <160824306+abbadasarishravani@users.noreply.github.com>
2024-02-26 21:43:33 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,15 @@
+//This program is to print the 1 to 10 natural numbers
+import java.io.*;
+class numbers
+{
+public static void main(String args[])
+{
+int n = 10;
+int i= 1;
+while(i<=n)
+{
+System.out.println(i);
+i++;
+}
+}//main
+}//Numbers
