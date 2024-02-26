3c950f9c71f1013d164045d32b2aaf9a4dc4c6f3
abbadasarishravani <160824306+abbadasarishravani@users.noreply.github.com>
2024-02-26 21:47:43 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,9 @@
+//This program is used to concatenates given strings
+import java.io.*;
+class second
+{
+public static void main(String args[])
+{
+System.out.println("HI"+"\t"+"Hello"+"\t"+"Bye");
+}//main
+}//second
