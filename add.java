9b22034fef163fcd5cca516be2c81ad5073e1cf0
abbadasarishravani <160824306+abbadasarishravani@users.noreply.github.com>
2024-02-26 21:42:45 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,11 @@
+//This program is used to perform addition of two numbers
+import java.io.*;
+class add
+{
+public static void main(String args[])
+{
+int a=20;
+int b=30;
+System.out.println(a+b);
+}//main
+}//add
